31046582eaf84bf7a7dac6d88b73daba782eb602
ssthouse <ssthouse@163.com>
2017-01-28 19:29:29 +0800

    Test if-else usage again

--- src/components/IfElse.vue
+++ src/components/IfElse.vue
@@ -5,6 +5,18 @@
     <!--<p v-else-if="input num == 10">input num == 10</p>-->
     <p v-else="input num < 10">input num < 10</p>
     <input v-model="input_num">
+    <br>
+    <br>
+
+    <div v-if="isUsernameLog">
+      <label>Username:</label>
+      <input placeholder="Enter your name">
+    </div>
+    <div v-else>
+      <label>Email:</label>
+      <input placeholder="Enter your email">
+    </div>
+    <input type="checkbox" v-model="isUsernameLog">Use email to login?</input>
   </div>
 </template>
 
@@ -13,7 +25,8 @@
     name: 'if-else',
     data () {
       return {
-        input_num: 10
+        input_num: 10,
+        isUsernameLog: true
       }
     }
   }
